9a9b1afbd29af8dca44b25b680cefa9b42c0584b
Marius Ghita <mhitza@gmail.com>
2013-05-18 04:26:05 +0300

    Add MVar's to keep the fork alive while forking multiway communication

--- Main.hs
+++ Main.hs
@@ -7,18 +7,32 @@ module Main where
     import Control.Monad 
     import Control.Proxy
 
+    import Control.Concurrent.MVar
+    import qualified Data.ByteString as B
+
 
     readDispose () = runIdentityP $ forever $ do
         readValue <- request ()
+        lift $ B.putStrLn readValue
         respond readValue
         return ()
 
+
     main :: IO ()
     main = do
+        m' <- newEmptyMVar
         (host:port:bindport:_) <- getArgs
         serve HostAny bindport $ \(bindSocket, _) -> do
             connect host port $ \(serviceSocket, _) -> do
-                forkIO $ do runProxy $ socketReadS 100 bindSocket >-> readDispose >-> socketWriteD serviceSocket 
-                forkIO $ do runProxy $ socketReadS 100 serviceSocket >-> readDispose >-> socketWriteD bindSocket 
+                c1 <- newEmptyMVar
+                c2 <- newEmptyMVar
+                forkIO $ do
+                    runProxy $ socketReadS 4096 bindSocket >-> readDispose >-> socketWriteD serviceSocket 
+                    putMVar c1 True
+                forkIO $ do
+                    runProxy $ socketReadS 4096 serviceSocket >-> readDispose >-> socketWriteD bindSocket 
+                    putMVar c2 True
+                takeMVar c1
                 return ()
-                    
+        takeMVar m'
+        return ()
