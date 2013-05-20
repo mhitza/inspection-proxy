f2cf6663bd1fbd6c8b5cf83859b07bdcf9583ec0
Marius Ghita <mhitza@gmail.com>
2013-05-20 22:00:30 +0300

    Wrap in void do bindings which we do not care about

--- Main.hs
+++ Main.hs
@@ -5,6 +5,7 @@ module Main where
     import Control.Proxy.TCP
     import Control.Proxy
 
+    import Control.Monad (void)
     import qualified Control.Concurrent.Async as Async
     import qualified Data.ByteString.Char8 as B
 
@@ -13,11 +14,7 @@ module Main where
     readDispose () = runIdentityP $ forever $ do
         readValue <- request ()
         lift $ B.putStrLn readValue
-        respond readValue >> return ()
-
-
-    async_ :: IO a -> IO ()
-    async_ f = Async.async f >> return ()
+        void $ respond readValue
 
 
     main :: IO ()
@@ -25,6 +22,6 @@ module Main where
         (host:port:bindport:_) <- getArgs
         serve HostAny bindport $ \(bindSocket, _) -> 
             connect host port $ \(serviceSocket, _) -> do
-                async_ $ runProxy $ socketReadS 4096 bindSocket >-> readDispose >-> socketWriteD serviceSocket 
+                void $ Async.async $ runProxy $ socketReadS 4096 bindSocket >-> readDispose >-> socketWriteD serviceSocket 
                 runProxy $ socketReadS 4096 serviceSocket >-> readDispose >-> socketWriteD bindSocket 
         return ()
