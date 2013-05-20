9f45b25ab6b021761331c875de596f9ecf58f7c7
Marius Ghita <mhitza@gmail.com>
2013-05-20 23:07:22 +0300

    Add basic argument parsing

--- Main.hs
+++ Main.hs
@@ -1,24 +1,23 @@
-{-# LANGUAGE DeriveDataTypeable #-}
 module Main where
 
 
     import System.Environment (getArgs)
     import Control.Proxy.TCP
     import Control.Proxy
-    import System.Console.CmdArgs
+    import System.Console.CmdArgs.Explicit
 
     import Control.Monad (void)
     import qualified Control.Concurrent.Async as Async
     import qualified Data.ByteString.Char8 as B
 
-
-    data Configuration = Configuration
-                         { onlyClient :: Bool
-                         , onlyServer :: Bool
-                         } deriving (Show, Data, Typeable)
-
-
-    defaultConfiguration = Configuration { onlyServer = True, onlyClient = False }
+    arguments :: Mode [(String,String)]
+    arguments = mode "inspection-proxy" [] "" (flagArg (upd "proxy-setup") "proxy-port server port")
+        [ flagNone ["only-server"] (("only-server",""):) "print only server response activity"
+        , flagNone ["only-client"] (("only-client",""):) "print only client request activity"
+        , flagHelpSimple (("help",""):)
+        , flagVersion (("version",""):)
+        ]
+        where upd msg x v = Right $ (msg,x):v
 
 
     readDispose :: Proxy p => () -> p () B.ByteString b' B.ByteString IO r
@@ -30,11 +29,13 @@ module Main where
 
     main :: IO ()
     main = do
-        conf <- cmdArgs defaultConfiguration
-        print conf
-        (host:port:bindport:_) <- getArgs
-        serve HostAny bindport $ \(bindSocket, _) -> 
-            connect host port $ \(serviceSocket, _) -> do
-                void $ Async.async $ runProxy $ socketReadS 4096 bindSocket >-> readDispose >-> socketWriteD serviceSocket 
-                runProxy $ socketReadS 4096 serviceSocket >-> readDispose >-> socketWriteD bindSocket 
-        return ()
+        args <- processArgs arguments 
+        if elem ("help","") args
+            then print $ helpText [] HelpFormatDefault arguments
+            else do
+                (host:port:bindport:_) <- getArgs
+                serve HostAny bindport $ \(bindSocket, _) -> 
+                    connect host port $ \(serviceSocket, _) -> do
+                        void $ Async.async $ runProxy $ socketReadS 4096 bindSocket >-> readDispose >-> socketWriteD serviceSocket 
+                        runProxy $ socketReadS 4096 serviceSocket >-> readDispose >-> socketWriteD bindSocket 
+                return ()
