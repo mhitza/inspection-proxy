62319389084d483e5cda0f04080385619c36df06
Marius Ghita <mhitza@gmail.com>
2013-08-29 00:14:21 +0300

    Starting point on json transformer

--- JsonPrint.hs
+++ JsonPrint.hs
@@ -1,10 +1,16 @@
 module Main where
 
     import Data.JSON
+    import Data.Aeson.Types
+    import qualified Data.HashMap.Strict as HM
+    import qualified Data.Vector as V
+    import qualified Data.ByteString as B
 
-    import Data.ByteString
-    import Prelude hiding (getContents)
+    
+    structuralJsonTransform (Object o) = concatMap (structuralJsonTransform . snd) $ HM.toList o
+    structuralJsonTransform (Array a)  = concatMap structuralJsonTransform $ V.toList a
+    structuralJsonTransform v          = [v]
 
 
     main :: IO ()
-    main = getContents >>= print . parseJSON
+    main = B.getContents >>= print . fmap structuralJsonTransform . Data.JSON.parseJSON
